3d1fdd9e2c069be83869694e66d0150fe73e1c21
unknown <bmchucop@gmail.com>
2023-07-20 14:07:03 -0500

    Resuelto

--- 07-Testing/homework/01 - Exercises/Part 01/levels.js
+++ 07-Testing/homework/01 - Exercises/Part 01/levels.js
@@ -1,9 +1,38 @@
-const levelOne = (a, b) => {};
+const levelOne = (a, b) => {
+    return a + b
+};
 
-const levelTwo = (letras) => {};
+const levelTwo = (letras) => {
+    if(letras.length < 2){
+        return letras
+    }
+    if(letras.length === 2){
+        return letras[0]
+    }
+    if(letras.length > 2){
+        let result = [];
 
-const levelThree = (a, b) => {};
+        for(let i = 0; i < letras.length; i++){
+            if(i % 2 === 0) result.push(letras[i])
+        }
+        return result.join("")
+    }
+};
 
-const levelFour = (num) => {};
+const levelThree = (a, b) => {
+   return [...a, ...b].sort()
+};
+
+const levelFour = (num) => {
+    let str = num.toString().split("");
+
+    let numbers = str.map(string => Number(string));
+
+    let result = numbers.reduce((acc, num) => acc + num);
+
+    let resultReverse = result.toString().split("").reverse().join("");
+
+    return result * Number(resultReverse) === num;
+};
 
 module.exports = { levelOne, levelTwo, levelThree, levelFour };
